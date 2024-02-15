8100af5e63ac9bca0bee42e8eb36c88a7edfee5a
Barmaley_YT <kunevich-evgeniyy@rambler.ru>
2024-02-15 12:18:32 +0300

    Searcher

--- searcher.py
+++ searcher.py
@@ -2,6 +2,7 @@ import uuid
 import asyncio
 import aiofiles
 import pyshorteners
+from typing import Union
 from urllib.parse import quote_plus, unquote_plus
 from bs4 import BeautifulSoup as Bs
 import textwrap
@@ -14,7 +15,13 @@ from aiohttp.client_exceptions import (
 from lxml.html import fromstring
 
 
-async def get_requests_to_url(url: str) -> str:
+async def get_requests_to_url(url: str) -> Union[str, None]:
+    """
+    Get page any found url in search system
+
+    :param url: str
+    :return: Union[str, None]
+    """
     headers = {"User-Agent": "Opera/9.80 (J2ME/MIDP; Opera Mini/9.80 (S60; SymbOS; "
                              "Opera Mobi/23.334; U; id) Presto/2.5.25 Version/10.54"}
     async with ClientSession() as session:
@@ -23,7 +30,13 @@ async def get_requests_to_url(url: str) -> str:
                 return await resp.text()
 
 
-async def get_page_search(search_url: str) -> str:
+async def get_page_search(search_url: str) -> Union[str, None]:
+    """
+    Requests to search system and get code page
+
+    :param search_url: str
+    :return: Union[str, None]
+    """
     headers = {"User-Agent": "Opera/9.80 (J2ME/MIDP; Opera Mini/9.80 (S60; SymbOS; "
                              "Opera Mobi/23.334; U; id) Presto/2.5.25 Version/10.54"}
     async with ClientSession() as session:
@@ -34,27 +47,24 @@ async def get_page_search(search_url: str) -> str:
                 return await resp.text()
 
 
-async def search_google(search_string: str) -> str:
-    search_url = f"https://www.google.com/search?q={quote_plus(search_string)}"
-    return await get_page_search(search_url)
+async def search_service(search_url: str, search_string: str) -> str:
+    """
+    Get page in search system
+
+    :param search_url: str
+    :param search_string: str
+    :return: str
+    """
+    return await get_page_search(search_url.format(quote_plus(search_string)))
 
 
-async def search_yandex(search_string: str) -> str:
-    search_url = f"https://yandex.ru/search?text={quote_plus(search_string)}"
-    return await get_page_search(search_url)
+async def find_url_check(response: str) -> list:
+    """
+    Get urls in search page
 
-
-async def search_bing(search_string: str) -> str:
-    search_url = f"https://www.bing.com/search?q={quote_plus(search_string)}"
-    return await get_page_search(search_url)
-
-
-async def search_duckduckgo(search_string: str) -> str:
-    search_url = f"https://duckduckgo.com/?q={quote_plus(search_string)}"
-    return await get_page_search(search_url)
-
-
-async def find_url_to_check(response: str) -> list:
+    :param response: str
+    :return: list
+    """
     soup = Bs(response, "html.parser")
     links = soup.findAll("a")
     url_sets = {unquote_plus(temp.replace('//duckduckgo.com/l/?uddg=', '')) for i in links
@@ -64,13 +74,38 @@ async def find_url_to_check(response: str) -> list:
         return list(url_list)
 
 
-async def parse_url_and_title(search_string: str) -> None:
-    search_system = [search_duckduckgo, search_google, search_yandex, search_bing]
+async def parse_page_short_view(search_string: str) -> None:
+    """
+    Crab page title to get short info
+
+    Enter you`re questions >> music free
+
+    console output:
+
+    Search_system : [duckduckgo] Title : YouTube	 URL сайта : https://clck.ru/38pemx
+    ...
+    Search_system : [google] Title : Условия использования Google – Политика конфиденциальности и Условия использования – Google	 URL сайта : https://clck.ru/38pZh8
+    ...
+    Search_system : [yandex] Title : Law	 URL сайта : https://clck.ru/38perp
+    ...
+    Search_system : [bing] Title : Overview - Microsoft Advertising	 URL сайта : https://clck.ru/38pZhs
+
+    :param search_string: str
+
+    :return: None
+    """
+
+    search_system = {
+        "duckduckgo": "https://duckduckgo.com/?q={}",
+        "google": "https://www.google.com/search?q={}",
+        "bing": "https://www.bing.com/search?q={}",
+        "yandex": "https://yandex.ru/search?text={}",
+    }
     date_from_search = []
-    for search in search_system:
-        response_search_system = await search(search_string)
+    for search_sys, search_url in search_system.items():
+        response_search_system = await search_service(search_url, search_string)
         if response_search_system:
-            urls_list = await find_url_to_check(response_search_system)
+            urls_list = await find_url_check(response_search_system)
             if urls_list:
                 for url in urls_list:
                     try:
@@ -83,9 +118,9 @@ async def parse_url_and_title(search_string: str) -> None:
                         shortened = textwrap.shorten(text=original, width=150)
                         title = wrapper.fill(text=shortened)
                         date_from_search.append(
-                            f"Search_system: [{search.__name__.replace('search_', '')}] "
-                            f"Заголовок: {title}\t "
-                            f"URL сайта: {pyshorteners.Shortener().clckru.short(url)}"
+                            f"Search_system : [{search_sys}] "
+                            f"Title : {title}\t "
+                            f"URL сайта : {pyshorteners.Shortener().clckru.short(url)}"
                         )
                     except (
                             TypeError,
@@ -105,4 +140,4 @@ async def parse_url_and_title(search_string: str) -> None:
         await f.write('\n'.join(date_from_search))
 
 
-asyncio.run(parse_url_and_title(input('Enter you`re questions >> ')))
+asyncio.run(parse_page_short_view(input('Enter you`re questions >> ')))
